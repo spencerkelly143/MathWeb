3b5cbf5baa84bc7786a15871223aaf64aabddc5f
Spencer Kelly <sek2@queensu.ca>
2019-06-20 17:40:23 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import './ui.css'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 function App() {
+  const [rectClick, setRectClick] = React.useState(false)
+  const [cClick, setCClick] = React.useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='container-fluid'>
+      <div className='header row'>
+        <div className='col'>
+          <div className='header paper'>
+            <h1>Text.</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='container-fluid'>
+    <div className='row'>
+    <div className="col-3">
+      <div className="paper side">
+        <div className="header">
+          <h1>Menu</h1>
+        </div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
+    </div>
+    <div className = 'col-6'>
+      <div className='row'>
+        <div className="col-6">
+          <div className="paper top">
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="paper top"></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="body paper">
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
